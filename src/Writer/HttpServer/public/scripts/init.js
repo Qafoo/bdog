72f46ec5e906dba9b725bd094bb4fe76af405ec8
Jakob Westhoff <jakob@westhoffswelt.de>
2012-04-08 17:03:56 +0200

    Made Faye and jQuery hard dependencies

--- src/Writer/HttpServer/public/scripts/init.js
+++ src/Writer/HttpServer/public/scripts/init.js
@@ -1,7 +1,14 @@
-require({
-  paths: {
-      'lib': '../lib',
-      'faye': '../faye',
-      'jquery': 'jquery-1.7.2.min'
-  }
-}, ['cs!lib/main']);
+require(
+    {
+        paths: {
+            'lib': '../lib',
+            'faye': '../faye',
+            'jquery': 'jquery-1.7.2.min'
+        }
+    },
+    [
+        'faye',
+        'jquery',
+        'cs!lib/main'
+    ]
+);
